87589abbeea32a3725178b5a1a764bdc1dea2939
yamadamizuki <yamadamizuki@yamadamukinoair>
2023-04-11 17:05:02 +0900

    task1一次コミット最新

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -51,24 +51,24 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）
-		if(password.length() >= 8 && password.matches("[0-9a-zA-Z]+")) {
-			if(password.equals(passwordForCheck)) {
-		
-		// パラメータで受け取ったアカウント情報をDtoに格納する。
-		UserInfo userInfo = new UserInfo();
-		userInfo.setEmail(email);
-		userInfo.setPassword(password);
-		usersService.registUser(userInfo);
-		return "redirect:/login";
-			}else {
-				model.addAttribute("errorMessage","パスワードが一致しません。");
+		if (password.length() >= 8 && password.matches("[0-9a-zA-Z]+")) {
+			if (password.equals(passwordForCheck)) {
+
+				// パラメータで受け取ったアカウント情報をDtoに格納する。
+				UserInfo userInfo = new UserInfo();
+				userInfo.setEmail(email);
+				userInfo.setPassword(password);
+				usersService.registUser(userInfo);
+				return "redirect:/login";
+			} else {
+				model.addAttribute("errorMessage", "パスワードが一致しません。");
 				return "createAccount";
-			    }
-		}else {
+			}
+		} else {
 			System.out.println("パスワードは８文字以上かつ半角英数字に設定してください。");
 			return "createAccount";
 		}
-		    
+
 	}
 
 }
